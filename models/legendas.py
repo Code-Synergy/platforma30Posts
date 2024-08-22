5f4c43d430c45bb55c0559c9cbebb09dc499a140
Jean DR Pires <jdrpires@gmail.com>
2024-08-22 20:35:14 -0300

    Integração com GPT

--- models/legendas.py
+++ models/legendas.py
@@ -6,15 +6,23 @@ legendas_bp = Blueprint('legendas', __name__)
 class Legenda(db.Model):
     __tablename__ = 'legendas'
 
-    id_legenda = db.Column(db.Integer, primary_key=True)
-    id_form = db.Column(db.Integer, db.ForeignKey('formulario_cliente.form_id'), primary_key=True)
-    dia_post = db.Column(db.String(10), primary_key=True)
+    id_legenda = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_form = db.Column(db.Integer, db.ForeignKey('formulario_cliente.form_id'), nullable=False)
+    dia_post = db.Column(db.String(10), nullable=False)
     ds_legenda = db.Column(db.Text, nullable=False)
 
     def __init__(self, id_form, dia_post, ds_legenda):
         self.id_form = id_form
         self.dia_post = dia_post
         self.ds_legenda = ds_legenda
+
+    def serialize(self):
+        return {
+            'id_legenda': self.id_legenda,
+            'id_form': self.id_form,
+            'dia_post': self.dia_post,
+            'ds_legenda': self.ds_legenda
+        }
 
 @legendas_bp.route('/', methods=['GET'])
 def get_legendas():
@@ -35,17 +43,15 @@ def add_legenda():
 
 @legendas_bp.route('/<int:id_legenda>/<int:id_form>/<string:dia_post>', methods=['PUT'])
 def update_legenda(id_legenda, id_form, dia_post):
-    legenda = Legenda.query.get_or_404((id_legenda, id_form, dia_post))
+    legenda = Legenda.query.filter_by(id_legenda=id_legenda, id_form=id_form, dia_post=dia_post).first_or_404()
     data = request.get_json()
-
     legenda.ds_legenda = data.get('ds_legenda', legenda.ds_legenda)
-
     db.session.commit()
     return jsonify(legenda.serialize())
 
 @legendas_bp.route('/<int:id_legenda>/<int:id_form>/<string:dia_post>', methods=['DELETE'])
 def delete_legenda(id_legenda, id_form, dia_post):
-    legenda = Legenda.query.get_or_404((id_legenda, id_form, dia_post))
+    legenda = Legenda.query.filter_by(id_legenda=id_legenda, id_form=id_form, dia_post=dia_post).first_or_404()
     db.session.delete(legenda)
     db.session.commit()
     return '', 204
