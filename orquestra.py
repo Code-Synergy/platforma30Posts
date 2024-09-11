91812c3ba88572c14b593da664b0aaab3b4e0218
Alberto Ogura <alberto.ogura@codesynergy.com.br>
2024-09-11 11:49:38 -0300

    Ajuste do orquestrador

--- orquestra.py
+++ orquestra.py
@@ -1,15 +1,11 @@
 from flask import Blueprint, request, jsonify, current_app
-from marshmallow import ValidationError
-from werkzeug.security import generate_password_hash, check_password_hash
-import jwt
-import datetime
+import datetime  # Importando o módulo corretamente
 
 from models import db
 from models.formulario_cliente import FormularioCliente
 from models.negocios import Negocio
 from models.ordens_de_servico import OrdemDeServico
-from models.pedido import pedidos_bp, Pedido
-from models.send_form import cliente_schema
+from models.pedido import Pedido
 from utils.token_verify import token_required
 
 orquestra_bp = Blueprint('orquestra', __name__)
@@ -18,18 +14,9 @@ orquestra_bp = Blueprint('orquestra', __name__)
 @token_required
 def GeraPedido(token_data):
 
-    try:
-        data = request.get_json()
-
-        # Validação do body da requisição
-        cliente_schema.load(data)
-
-    except ValidationError as err:
-        return jsonify(err.messages), 400
-
     user_id = token_data.get('user_id')
 
-    #Adicona um negócio
+    # Adiciona um negócio
     negocio = Negocio(
         cliente_id=user_id,
         nome_negocio='batata',
@@ -37,9 +24,9 @@ def GeraPedido(token_data):
         ativo=True
     )
     db.session.add(negocio)
+    db.session.commit()
 
-    #Adiciona o pedido
-    data = request.get_json()
+    # Adiciona o pedido
     pedido = Pedido(
         negocio_id=negocio.negocio_id,
         descricao='Post Free',
@@ -48,37 +35,33 @@ def GeraPedido(token_data):
         ativo=True  # Incluindo ativo
     )
     db.session.add(pedido)
+    db.session.commit()
 
-    #Adiciona Ordem de serviço
+    # Adiciona Ordem de serviço
     ordem = OrdemDeServico(
         pedido_id=pedido.pedido_id,
         descricao='Post Free',
-        data=datetime,
+        data=datetime.datetime.now(),  # Usando a data corretamente
         usuario_id=user_id,
         workflow_id=1,
-        id_form='',
         id_negocio=negocio.negocio_id,
-        indicacao='',
-        agencia='',
         id_produto=1,
-        prazointerno=datetime,
-        prazoexterno=datetime,
-        entrega=datetime,
-        mensal='',
-        responsavel='ZOE',
-        observacao=''
+        prazointerno=datetime.datetime.now(),  # Usando a data corretamente
+        prazoexterno=datetime.datetime.now(),  # Usando a data corretamente
+        entrega=datetime.datetime.now(),  # Usando a data corretamente
+        mensal=False,
     )
     db.session.add(ordem)
+    db.session.commit()
 
-    #Adiciona o Formulário para preechimento
+    # Adiciona o Formulário para preenchimento
     formulario = FormularioCliente(
-        os_id=ordem.ordem_id,
-        whatsapp='',
-        email='email'
+        ordem_id=ordem.ordem_id,
+        nome_cliente='',
+        whatsapp_cliente='',
+        email_cliente='email'
     )
     db.session.add(formulario)
-
     db.session.commit()
 
     return jsonify({'FormID': formulario.id_form}), 201
-
