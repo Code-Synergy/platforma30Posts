68f03c3685565c12283499e49aed976682a4d1b1
Jean DR Pires <jdrpires@gmail.com>
2024-09-13 22:14:55 -0300

    Subida de Fluxos Free

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from models import Gepeto_Zoe_V5, db, clientes, pedido, ordens_de_servico, negocios, send_form, validar_id_form, \
-    workflow, legendas, tipo_cliente, user, disparar
+    workflow, legendas, tipo_cliente, user, disparar, Gepeto_Zoe_Fluxo1
 from config import Config
 from flask_cors import CORS
 import orquestra
@@ -36,6 +36,10 @@ app.register_blueprint(Gepeto_Zoe_V5.zoe_bp, url_prefix='/zoe')
 app.register_blueprint(orquestra.orquestra_bp, url_prefix='/orquestra')
 
 app.register_blueprint(disparar.disparar_bp, url_prefix='/disparar')
+
+
+app.register_blueprint(Gepeto_Zoe_Fluxo1.zoeFluxo1_bp, url_prefix='/fluxo1')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
