849b83cdbf576c4887566a1bce8434e68274754a
Jean DR Pires <jdrpires@gmail.com>
2024-09-25 15:45:09 -0300

    Legenda Higor

--- models/legendas.py
+++ models/legendas.py
@@ -13,7 +13,6 @@ legendas_bp = Blueprint('legendas', __name__)
 
 class Legenda(db.Model):
     __tablename__ = 'legendas'
-
     id_legenda = db.Column(db.Integer, primary_key=True, autoincrement=True)
     id_form = db.Column(db.Integer, nullable=False)
     dia_post = db.Column(db.Integer, nullable=False)
@@ -25,9 +24,10 @@ class Legenda(db.Model):
     bl_planner = db.Column(db.Boolean, default=False, nullable=True)
     ds_headline = db.Column(db.Text, nullable=False)
     ds_hashtag = db.Column(db.Text, nullable=False)
+    ds_canva = db.Column(db.Text, nullable=True)
 
     def __init__(self, id_form, dia_post, ds_legenda, img_legenda=None, bl_aprovado=False, bl_revisar=False,
-                 ds_revisao=None, bl_planner=False, ds_headline=None, ds_hashtag=None):
+                 ds_revisao=None, bl_planner=False, ds_headline=None, ds_hashtag=None, ds_canva=None):
         self.id_form = id_form
         self.dia_post = dia_post
         self.ds_legenda = ds_legenda
@@ -38,6 +38,7 @@ class Legenda(db.Model):
         self.bl_planner = bl_planner
         self.ds_headline = ds_headline
         self.ds_hashtag = ds_hashtag
+        self.ds_canva = ds_canva
 
     def serialize(self):
         return {
@@ -51,7 +52,8 @@ class Legenda(db.Model):
             'ds_revisao': self.ds_revisao,
             'bl_planner': self.bl_planner,
             'ds_headline': self.ds_headline,
-            'ds_hashtag': self.ds_hashtag
+            'ds_hashtag': self.ds_hashtag,
+            'ds_canva': self.ds_canva
         }
 
 
@@ -111,7 +113,8 @@ def geraLegenda(data):
         ds_revisao=data.get('ds_revisao', None),
         bl_planner=data.get('bl_planner', False),
         ds_headline=data.get('ds_headline', None),
-        ds_hashtag=data.get('ds_hashtag', None)
+        ds_hashtag=data.get('ds_hashtag', None),
+        ds_canva=data.get('ds_canva', None)
     )
 
     try:
@@ -124,6 +127,31 @@ def geraLegenda(data):
         print('***********************************************')
         print('COMITOU A LEGENDA')
         print('***********************************************')
+
+        print('BORA AVISAR CLIENTE: ....')
+        print("ENVIAR MENSAGEM NO WHATS.....")
+        telefone = consultaTele(form_cliente)
+        URLTigor = "https://tigor.itlabs.app/wpp/api"
+        payload = {
+            "app": "3bd82d2e-3077-4226-a366-1338eb3ed589",
+            "number": telefone,
+            "message": "Parabens! Seu post esta em produção.\n Obrigado por escolher a 30 Posts. \n Após apreciar seu post grátis o que acha de conhecer nossos planos ?",
+            "type": "text",
+            "url": ""
+        }
+
+        headers = {
+            "Content-Type": "application/json"  # Define que o conteúdo enviado é JSON
+        }
+
+        responseWhats = requests.post(URLTigor, json=payload, headers=headers)
+
+        if responseWhats.status_code == 200 or responseWhats.status_code == 201:
+            print('Cliente informado com sucesso!')
+        else:
+            print('Erro ao informar cliente:')
+            print(responseWhats.text)
+
         # Retorna a legenda adicionada com sucesso
         return jsonify(legenda.serialize()), 201
     except Exception as e:
@@ -149,6 +177,7 @@ def update_legenda(id_legenda):
     legenda.bl_planner = data.get('bl_planner', legenda.bl_planner)
     legenda.ds_headline = data.get('ds_headline', legenda.ds_headline)
     legenda.ds_hashtag = data.get('ds_hashtag', legenda.ds_hashtag)
+    legenda.ds_canva = data.get('ds_canva', legenda.ds_canva)
 
     db.session.commit()
     return jsonify(legenda.serialize())
